54f9441cc3c1df455b8d45fc64343d0f40782229
phongttgcd <phongttgcd201885@fpt.edu.vn>
2024-02-24 14:28:40 +0700

    Add project files.

---
+++ COMP1640-WebDev/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP1640_WebDev.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
