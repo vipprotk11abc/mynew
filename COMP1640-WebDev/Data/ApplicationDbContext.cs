5f7e05d3df0eb723a923bcc331bcb7dc44caacde
hoanglongle77 <100249743+hoanglongle77@users.noreply.github.com>
2024-02-24 22:41:43 +0700

    Add role class, and seeding roles to database

--- COMP1640-WebDev/Data/ApplicationDbContext.cs
+++ COMP1640-WebDev/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using COMP1640_WebDev.Ultils;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace COMP1640_WebDev.Data
@@ -8,6 +10,47 @@ namespace COMP1640_WebDev.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            this.SeedRoles(builder);
+        }
+        private void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData(
+             new IdentityRole()
+             {
+                 Name = Role.ADMIN,
+                 ConcurrencyStamp = "1",
+                 NormalizedName = Role.ADMIN
+             },
+            new IdentityRole()
+            {
+                Name = Role.MARKETING_MANAGER,
+                ConcurrencyStamp = "2",
+                NormalizedName = Role.MARKETING_MANAGER
+            },
+            new IdentityRole()
+            {
+                Name = Role.MARKETING_COORDINATOR,
+                ConcurrencyStamp = "3",
+                NormalizedName = Role.MARKETING_COORDINATOR
+            },
+            new IdentityRole()
+            {
+                Name = Role.STUDENT,
+                ConcurrencyStamp = "4",
+                NormalizedName = Role.STUDENT
+            },
+            new IdentityRole()
+            {
+                Name = Role.GUEST,
+                ConcurrencyStamp = "5",
+                NormalizedName = Role.GUEST
+            }
+            );
         }
     }
 }
