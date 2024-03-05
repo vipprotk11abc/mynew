2661f3c93b2a41e3418beabf95eaa3f60f65b5c1
minhTriet254 <151365637+minhTriet254@users.noreply.github.com>
2024-03-06 01:48:18 +0700

    update logic time contribution

--- COMP1640-WebDev/Repositories/ContributionRepository.cs
+++ COMP1640-WebDev/Repositories/ContributionRepository.cs
@@ -1,6 +1,7 @@
 ﻿using COMP1640_WebDev.Data;
 using COMP1640_WebDev.Models;
 using COMP1640_WebDev.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 namespace COMP1640_WebDev.Repositories
@@ -15,22 +16,33 @@ namespace COMP1640_WebDev.Repositories
         }
         public async Task<Contribution> CreateContribution(Contribution contribution)
         {
-            Contribution contributionToCreate = new()
+            var academicYear = await _dbContext.AcademicYears.SingleOrDefaultAsync(a => a.Id == contribution.AcademicYearId);
+
+            if (contribution.SubmissionDate > academicYear.ClosureDate)
             {
-                Title = contribution.Title,
-                Document= contribution.Document,
-                Image = contribution.Image,
-                SubmissionDate = contribution.SubmissionDate,
-                IsEnabled = contribution.IsEnabled,
-                IsSelected = contribution.IsSelected,
-                Comment = contribution.Comment,
-                CommentDate = contribution.CommentDate,
-            };
+                return null;
+            }
+            else 
+            {
+                Contribution contributionToCreate = new()
+                {
+                    Title = contribution.Title,
+                    Document = contribution.Document,
+                    Image = contribution.Image,
+                    SubmissionDate = contribution.SubmissionDate,
+                    AcademicYearId = contribution.AcademicYearId,
+                    UserId = contribution.UserId,
+                    IsEnabled = contribution.IsEnabled,
+                    IsSelected = contribution.IsSelected,
+                    Comment = contribution.Comment,
+                    CommentDate = contribution.CommentDate,
+                };
+                var result = await _dbContext.Contributions.AddAsync(contributionToCreate);
+                await _dbContext.SaveChangesAsync();
 
-            var result = await _dbContext.Contributions.AddAsync(contributionToCreate);
-            await _dbContext.SaveChangesAsync();
-
-            return result.Entity;
+                return result.Entity;
+            }
+                       
         }
 
         public async Task<Contribution> GetContribution(string idContribution)
@@ -59,22 +71,32 @@ namespace COMP1640_WebDev.Repositories
             throw new NotImplementedException();
         }
 
+     
         public async Task<Contribution> UpdateContribution(string idContribution, Contribution contribution)
         {
-            var contributionInDb = await _dbContext.Contributions
-                .SingleOrDefaultAsync(e => e.Id == idContribution);
+            var academicYear = await _dbContext.AcademicYears.SingleOrDefaultAsync(a => a.Id == contribution.AcademicYearId);
 
-            if (contributionInDb == null)
+            if (contribution.SubmissionDate > academicYear.FinalDate)
             {
                 return null;
             }
+            else
+            {
+                var contributionInDb = await _dbContext.Contributions
+               .SingleOrDefaultAsync(e => e.Id == idContribution);
 
-            contributionInDb.Title = contribution.Title;
-            contributionInDb.Document = contribution.Document;
-            contributionInDb.Image = contribution.Image;
-            await _dbContext.SaveChangesAsync();
+                if (contributionInDb == null)
+                {
+                    return null;
+                }
 
-            return contribution;
+                contributionInDb.Title = contribution.Title;
+                contributionInDb.Document = contribution.Document;
+                contributionInDb.Image = contribution.Image;
+                await _dbContext.SaveChangesAsync();
+
+                return contribution;
+            }
         }
     }
 }
